e04493e262c0c53d7562d54830ab7496df5c9e92
Deepti Verma <deeptiverma455@gmail.com>
2020-11-21 18:21:19 +0530

    kjkjj

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,26 @@
 import React, {Component, useState} from "react";
-import {BrowserRouter, Link, Route, Switch } from 'react-router-dom';
+import {BrowserRouter, Link, Route, Switch, withRouter} from 'react-router-dom';
 import Home from './Home.js';
 import About from './About.js';
 import '../styles/App.css';
 
+
 class App extends Component {
+    // static propTypes = {
+    //     match: PropTypes.object.isRequired,
+    //     location: PropTypes.object.isRequired,
+    //     history: PropTypes.object.isRequired
+    //   }
     render() {
+        // const { match, location, history } = this.props
         return(
             <BrowserRouter>
+            
             <div id="main">
+            {/* <div>{location.pathname}</div> */}
             <Link to="/">HOME/</Link>
             <Link to="/about">ABOUT</Link>
+            <Link to="/about">Contact-Us</Link>
             <Switch>
                 <Route exact path='/'>
                     <Home />
@@ -27,4 +37,4 @@ class App extends Component {
 }
 
 
-export default App;
+export default withRouter(App);
